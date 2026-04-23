09de6e5d82d4aa349e09ef245e3f0906f695362d
Goutham Senthil Kumar <104307641+Goutham-Senthil@users.noreply.github.com>
2026-04-23 18:40:04 +0100

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.eoW = False
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.eoW = True
+        return
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.eoW
+        
+
+    def startsWith(self, word: str) -> bool:
+        curr = self.root
+
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+        
+        
