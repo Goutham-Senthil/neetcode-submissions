f83ae24cee17cd97d0a1f847674653e689e80a7f
Goutham Senthil Kumar <godlygoutham@gmail.com>
2026-06-18 09:01:59 +0100

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        # keep all same elements near each other
+        nums.sort()
+
+
+        def f(tmp,index):
+            if index == (n:=len(nums)):
+                # initial comment is wrong
+
+                res.append(tmp[::])
+                return 
+            
+            tmp.append(nums[index])
+            f(tmp,index+1) # 0-indexed
+            tmp.pop()
+            # within bounds and is a duplicate
+            while index+1 < n and nums[index+1] == nums[index]:
+                index+=1
+            
+            f(tmp,index+1)
+        
+        f([],0)
+        return res
